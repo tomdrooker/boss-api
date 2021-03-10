30657e1ddb37b62224163b5528b4783bc7188dac
Tom Rooker <tomdrooker@gmail.com>
2021-03-10 21:03:50 +0000

    minions put request

--- server/minions.js
+++ server/minions.js
@@ -28,8 +28,16 @@ minionsRouter.post("/", (req, res, next) => {
     res.status(201).send(newMinion);
 });
 
-minionsRouter.post("/", (req, res, next) => {
-
+minionsRouter.put("/:minionId", (req, res, next) => {
+    const minionToUpdate = {
+        id: req.body.id,
+        name: req.body.name,
+        title: req.body.title,
+        salary: Number(req.body.salary),
+        weaknesses: req.body.weaknesses
+    };
+    const updatedMinion = db.updateInstanceInDatabase("minions", minionToUpdate);
+    res.status(200).send(updatedMinion);
 });
 
 module.exports = minionsRouter;
