6fe4b6949ae2b386cc066785021992a9b5c0566f
Tom Rooker <tomdrooker@gmail.com>
2021-03-23 21:17:56 +0000

    Minions endpoints created

--- server/minions.js
+++ server/minions.js
@@ -37,13 +37,31 @@ minionsRouter.post("/", (req, res, next) => {
 });
 
 minionsRouter.put("/:minionId", (req, res, next) => {
-    const minionToUpdate = {
-        id: req.body.id,
-        name: req.body.name,
-        title: req.body.title,
-        salary: Number(req.body.salary),
-        weaknesses: req.body.weaknesses
-    };
-    const updatedMinion = updateInstanceInDatabase("minions", minionToUpdate);
-    res.status(200).send(updatedMinion);
+    const minionId = req.params.minionId;
+
+    if (getFromDatabaseById("minions", minionId)) {
+        const minionToUpdate = {
+            id: req.body.id,
+            name: req.body.name,
+            title: req.body.title,
+            salary: Number(req.body.salary),
+            weaknesses: req.body.weaknesses
+        };
+        const updatedMinion = updateInstanceInDatabase("minions", minionToUpdate);
+        res.status(200).send(updatedMinion);
+    }
+    else {
+        res.status(404).send();
+    }
+});
+
+minionsRouter.delete("/:minionId", (req, res, next) => {
+    const minionId = req.params.minionId;
+    if (getFromDatabaseById("minions", minionId)) {
+        deleteFromDatabasebyId("minions", minionId);
+        res.status(204).send();
+    }
+    else {
+        res.status(404).send();
+    }
 });
