6a738e01c030e5774245f6a238fc2561094ee47e
Tom Rooker <tomdrooker@gmail.com>
2021-03-24 20:24:27 +0000

    param middleware added in minions router

--- server/minions.js
+++ server/minions.js
@@ -10,19 +10,23 @@ const {
     deleteFromDatabasebyId,
   } = require("./db");
 
+  minionsRouter.param("minionId", (req, res, next, minionId) => {
+    const minion = getFromDatabaseById("minions", minionId);
+    if (minion != null) {
+        req.minion = minion;
+        next();
+    }
+    else {
+        res.status(404).send();
+    }
+  });
+
 minionsRouter.get("/", (req, res, next) => {
     res.send(getAllFromDatabase("minions"));
 });
 
 minionsRouter.get("/:minionId", (req, res, next) => {
-    const id = req.params.minionId;
-    const minion = getFromDatabaseById("minions", id);
-    if (minion != null) {
-        res.send(minion);
-    }
-    else {
-        res.status(404).send();
-    }
+    res.send(req.minion);
 });
 
 minionsRouter.post("/", (req, res, next) => {
@@ -37,31 +41,18 @@ minionsRouter.post("/", (req, res, next) => {
 });
 
 minionsRouter.put("/:minionId", (req, res, next) => {
-    const minionId = req.params.minionId;
-
-    if (getFromDatabaseById("minions", minionId)) {
-        const minionToUpdate = {
-            id: req.body.id,
-            name: req.body.name,
-            title: req.body.title,
-            salary: Number(req.body.salary),
-            weaknesses: req.body.weaknesses
-        };
-        const updatedMinion = updateInstanceInDatabase("minions", minionToUpdate);
-        res.status(200).send(updatedMinion);
-    }
-    else {
-        res.status(404).send();
-    }
+    const minionToUpdate = {
+        id: req.body.id,
+        name: req.body.name,
+        title: req.body.title,
+        salary: Number(req.body.salary),
+        weaknesses: req.body.weaknesses
+    };
+    const updatedMinion = updateInstanceInDatabase("minions", minionToUpdate);
+    res.status(200).send(updatedMinion);
 });
 
 minionsRouter.delete("/:minionId", (req, res, next) => {
-    const minionId = req.params.minionId;
-    if (getFromDatabaseById("minions", minionId)) {
-        deleteFromDatabasebyId("minions", minionId);
-        res.status(204).send();
-    }
-    else {
-        res.status(404).send();
-    }
+    deleteFromDatabasebyId("minions", minionId);
+    res.status(204).send();
 });
