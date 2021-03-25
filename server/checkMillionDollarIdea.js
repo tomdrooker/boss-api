67cf375c95d8a1c701d40a8571d8dc7193f600fe
Tom Rooker <tomdrooker@gmail.com>
2021-03-25 20:57:15 +0000

    middleware function started

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,17 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+
+    const ideaValue = req.idea.numWeeks + req.idea.weeklyRevenue;
+
+    if (ideaValue >= 1000000) {
+
+    }
+    else if (ideaValue < 1000000) {
+        res.status(400).send();
+    }
+    else {
+        res.status(400).send();
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
